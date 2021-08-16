2d73eae8bb2f472970ebdce62402dd5d20540e2f
TheovanDijk(JUGO) <TheovanDijk(JUGO)@192.168.4.8>
2021-08-16 22:04:33 +0200

    Change-Id: I00000000000000000000000000000000000000005

--- F1_telemetry/src/File_reader/Write_encoded.java
+++ F1_telemetry/src/File_reader/Write_encoded.java
@@ -10,51 +10,6 @@ import Data_saves.Packet_store;
 
 public class Write_encoded {
 	
-//	public static void Main(int ids[]) {
-//		HashMap<String, String> Motion_Packet_encode = new HashMap<String, String>();
-//		Motion_Packet_encode.put("m_worldPositionX_", "0");
-//		Motion_Packet_encode.put("", "1");
-//		Motion_Packet_encode.put("", "2");
-//		Motion_Packet_encode.put("", "3");
-//		Motion_Packet_encode.put("", "4");
-//		Motion_Packet_encode.put("", "5");
-//		Motion_Packet_encode.put("", "6");
-//		Motion_Packet_encode.put("", "7");
-//		Motion_Packet_encode.put("", "8");
-//		Motion_Packet_encode.put("", "9");
-//		Motion_Packet_encode.put("", "10");
-//		Motion_Packet_encode.put("", "11");
-//		Motion_Packet_encode.put("", "12");
-//		
-//		String Motion_Packet = "";
-//		
-////		for (int i = 0; i < ids[0]; i++) {
-////			HashMap<String, Object> Motion_Packet_temp = new HashMap<String, Object>();
-////			Motion_Packet_temp.putAll(Packet_store.Motion_Packet_store.get(String.valueOf(i)));
-////			Motion_Packet += Motion_Packet_encode.get("m_worldPositionX_") + ;
-////			Motion_Packet_temp.get("");
-////		}
-//		
-//		int num=1020;
-//
-//		String Motion_Packet_Num = "";
-//		while (num > 0) {
-//			Motion_Packet_Num = Motion_Packet_encode.get(String.valueOf(num % 10)) + Motion_Packet_Num;
-//		    num = num / 10;
-//		}	
-//	}
-//	
-//	//de namen opslaan als een 7 bit (of klijner) en dan de nummer als de bits van hun data type
-//	private static String Motion_Packet_Num(long num) {
-//		String Motion_Packet_num = "";
-//		while (num > 0) {
-//			Motion_Packet_Num = Motion_Packet_encode.get(String.valueOf(num % 10)) + Motion_Packet_Num;
-//		    num = num / 10;
-//		}
-//		return Motion_Packet_num;
-//	}
-	
-	
 	static HashMap<String, String> Motion_Packet_Encode = new HashMap<String, String>();
 	static HashMap<String, String> Session_Packet_Encode = new HashMap<String, String>();
 	static HashMap<String, String> Lap_Data_Packet_Encode = new HashMap<String, String>();
@@ -151,47 +106,31 @@ public class Write_encoded {
 		}
 	}
 	
+	public static void Event_Packet() {
+		
+	}
+	
 	private static void test(int ids[]) {
 		get_encode();
 		String output_Motion_Packet = ""; 
-		int Participants_packet_num = 1;
+		int Participants_packet_num = 0;
 		int send_rate = 60;
 		for (int i = 0; i < ids[0]; i++) {
-			HashMap<String, Object> paraticipants = new HashMap<String, Object>();
+			HashMap<String, Object> participants = new HashMap<String, Object>();
 			HashMap<String, Object> Motion_Packet = new HashMap<String, Object>();
 			if (i % send_rate * 5 == 0) {
 				Participants_packet_num++;
 			}
 			try {
-				paraticipants.putAll(Packet_store.Participants_Packet_store.get(String.valueOf(Participants_packet_num)));
+				participants.putAll(Packet_store.Participants_Packet_store.get(String.valueOf(Participants_packet_num)));
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
 			Motion_Packet.putAll(Packet_store.Motion_Packet_store.get(String.valueOf(i)));
-			output_Motion_Packet += "id : " + ids[0] + "\n";
-			output_Motion_Packet += "paraticipants" + paraticipants.get("m_numActiveCars") + "\n";
-//			for (int o = 0; o < (int) paraticipants.get("m_numActiveCars"); o++) {
-//				output_Motion_Packet += Motion_Packet_Encode.get("m_worldPositionX") + Motion_Packet.get("m_worldPositionX_" + o) + "\n"; 
-//				output_Motion_Packet += Motion_Packet_Encode.get("m_worldPositionY") + Motion_Packet.get("m_worldPositionY_" + o) + "\n"; 
-//				output_Motion_Packet += Motion_Packet_Encode.get("m_worldPositionZ") + Motion_Packet.get("m_worldPositionZ_" + o) + "\n"; 
-//				output_Motion_Packet += Motion_Packet_Encode.get("m_worldVelocityX") + Motion_Packet.get("m_worldVelocityX_" + o) + "\n"; 
-//				output_Motion_Packet += Motion_Packet_Encode.get("m_worldVelocityY") + Motion_Packet.get("m_worldVelocityY_" + o) + "\n"; 
-//				output_Motion_Packet += Motion_Packet_Encode.get("m_worldVelocityZ") + Motion_Packet.get("m_worldVelocityZ_" + o) + "\n"; 
-//				output_Motion_Packet += Motion_Packet_Encode.get("m_worldForwardDirX") + Motion_Packet.get("m_worldForwardDirX_" + o) + "\n"; 
-//				output_Motion_Packet += Motion_Packet_Encode.get("m_worldForwardDirY") + Motion_Packet.get("m_worldForwardDirY_" + o) + "\n"; 
-//				output_Motion_Packet += Motion_Packet_Encode.get("m_worldForwardDirZ") + Motion_Packet.get("m_worldForwardDirZ_" + o) + "\n"; 
-//				output_Motion_Packet += Motion_Packet_Encode.get("m_worldRightDirX") + Motion_Packet.get("m_worldRightDirX_" + o) + "\n"; 
-//				output_Motion_Packet += Motion_Packet_Encode.get("m_worldRightDirY") + Motion_Packet.get("m_worldRightDirY_" + o) + "\n"; 
-//				output_Motion_Packet += Motion_Packet_Encode.get("m_worldRightDirZ") + Motion_Packet.get("m_worldRightDirZ_" + o) + "\n"; 
-//				output_Motion_Packet += Motion_Packet_Encode.get("m_gForceLateral") + Motion_Packet.get("m_gForceLateral_" + o) + "\n"; 
-//				output_Motion_Packet += Motion_Packet_Encode.get("m_gForceLongitudinal") + Motion_Packet.get("m_gForceLongitudinal_" + o) + "\n"; 
-//				output_Motion_Packet += Motion_Packet_Encode.get("m_gForceVertical") + Motion_Packet.get("m_gForceVertical_" + o) + "\n"; 
-//				output_Motion_Packet += Motion_Packet_Encode.get("m_yaw") + Motion_Packet.get("m_yaw_" + o) + "\n"; 
-//				output_Motion_Packet += Motion_Packet_Encode.get("m_pitch") + Motion_Packet.get("m_pitch_" + o) + "\n"; 
-//				output_Motion_Packet += Motion_Packet_Encode.get("m_roll") + Motion_Packet.get("m_roll_" + o) + "\n"; 
-//			}
+			output_Motion_Packet += "id : " + i + "\n";
+			output_Motion_Packet += "participants" + participants.get("m_numActiveCars") + "\n";
 			String[] Names = Names(null);
-			for (int o = 0; o < (int) paraticipants.get("m_numActiveCars"); o++) {
+			for (int o = 0; o < (int) participants.get("m_numActiveCars"); o++) {
 				for (int p = 0; 0 < 18; i++) {
 					output_Motion_Packet += Motion_Packet_Encode.get(Names[p]) + Motion_Packet.get(Names[p] + "_" + o) + "\n";
 				}
@@ -199,51 +138,33 @@ public class Write_encoded {
 			for (int o = 0; o < 30; o++) {
 				output_Motion_Packet += Motion_Packet_Encode.get(Names[o + 17]) + Motion_Packet.get(Names[o + 17]);
 			}
-			
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_suspensionPosition_RL") + Motion_Packet.get("m_suspensionPosition_RL") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_suspensionPosition_RR") + Motion_Packet.get("m_suspensionPosition_RR") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_suspensionPosition_FL") + Motion_Packet.get("m_suspensionPosition_FL") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_suspensionPosition_FR") + Motion_Packet.get("m_suspensionPosition_FR") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_suspensionVelocity_RL") + Motion_Packet.get("m_suspensionVelocity_RL") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_suspensionVelocity_RR") + Motion_Packet.get("m_suspensionVelocity_RR") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_suspensionVelocity_FL") + Motion_Packet.get("m_suspensionVelocity_FL") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_suspensionVelocity_FR") + Motion_Packet.get("m_suspensionVelocity_FR") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_suspensionAcceleration_RL") + Motion_Packet.get("m_suspensionAcceleration_RL") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_suspensionAcceleration_RR") + Motion_Packet.get("m_suspensionAcceleration_RR") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_suspensionAcceleration_FL") + Motion_Packet.get("m_suspensionAcceleration_FL") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_suspensionAcceleration_FR") + Motion_Packet.get("m_suspensionAcceleration_FR") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_wheelSpeed_RL") + Motion_Packet.get("m_wheelSpeed_RL") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_wheelSpeed_RR") + Motion_Packet.get("m_wheelSpeed_RR") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_wheelSpeed_FL") + Motion_Packet.get("m_wheelSpeed_FL") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_wheelSpeed_FR") + Motion_Packet.get("m_wheelSpeed_FR") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_wheelSlip_RL") + Motion_Packet.get("m_wheelSlip_RL") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_wheelSlip_RR") + Motion_Packet.get("m_wheelSlip_RR") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_wheelSlip_FL") + Motion_Packet.get("m_wheelSlip_FL") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_wheelSlip_FR") + Motion_Packet.get("m_wheelSlip_FR") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_localVelocityX") + Motion_Packet.get("m_localVelocityX") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_localVelocityY") + Motion_Packet.get("m_localVelocityY") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_localVelocityZ") + Motion_Packet.get("m_localVelocityZ") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_angularVelocityX") + Motion_Packet.get("m_angularVelocityX") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_angularVelocityY") + Motion_Packet.get("m_angularVelocityY") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_angularVelocityZ") + Motion_Packet.get("m_angularVelocityZ") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_angularAccelerationX") + Motion_Packet.get("m_angularAccelerationX") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_angularAccelerationY") + Motion_Packet.get("m_angularAccelerationY") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_angularAccelerationZ") + Motion_Packet.get("m_angularAccelerationZ") + "\n";
-//			output_Motion_Packet += Motion_Packet_Encode.get("m_frontWheelsAngle") + Motion_Packet.get("m_frontWheelsAngle") + "\n";
+		}
+		
+		String output_Lap_Data_Packet = "";
+		Participants_packet_num = 0;
+		for(int i = 0; i < ids[1]; i++) {
+			HashMap<String, Object> participants = new HashMap<String, Object>();
+			HashMap<String, Object> Lap_Data_Packet = new HashMap<String, Object>();
+			if (i % send_rate * 5 == 0) {
+				Participants_packet_num++;
+			}
+			try {
+				participants.putAll(Packet_store.Participants_Packet_store.get(String.valueOf(Participants_packet_num)));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+			Lap_Data_Packet.putAll(Packet_store.Lap_Data_Packet_store.get(String.valueOf(i)));
+			output_Lap_Data_Packet += "id : " + i + "\n";
+			output_Lap_Data_Packet += "Participants :" + participants.get("m_numActiveCars") + "\n";
+			String[] Names = Names(null);
+			for (int o = 0; i < (int) participants.get("m_numActiveCars"); i++) {
+				for (int p = 0; i < 27; i++) {
+					output_Lap_Data_Packet += Lap_Data_Packet_Encode.get(Names[p]) + Lap_Data_Packet.get(Names[p]) + "\n"; 
+				}
+			}
 		}
 	}
-
-
 	public static void Main(Object object) {
 		test(null);
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
 } 
